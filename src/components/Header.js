d67ee0df6d28b1e0397a044cd3eb74944f90e795
Mohammad Shariq <m.shariq22@hotmail.com>
2020-09-12 10:59:52 +0500

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ export default function ButtonAppBar() {
         <h3>Excercise Tracker</h3>
       </div>
       <div className="header__nav">
-        <NavLink activeClassName="header__linkActive" className="header__link" to='/' exact>ExcerciseList</NavLink>
+        <NavLink activeClassName="header__linkActive" className="header__link" to='/' exact>ExcerciseLists</NavLink>
         <NavLink activeClassName="header__linkActive" className="header__link" to='/add' exact>AddExcercise</NavLink>
         <NavLink activeClassName="header__linkActive" className="header__link" to='/edit/id' exact>EditExcercise</NavLink>
         <NavLink activeClassName="header__linkActive" className="header__link" to='/user' exact>CreateUser</NavLink>
